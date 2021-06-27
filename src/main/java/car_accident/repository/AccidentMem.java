fb3f8c27105f331c61c3dd7e014588383655e0e3
Frostetskiy <nesterenko-nikita@bk.ru>
2021-06-27 11:07:54 +0300

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/car_accident/repository/AccidentMem.java
+++ src/main/java/car_accident/repository/AccidentMem.java
@@ -1,32 +1,44 @@
 package car_accident.repository;
 
 import car_accident.entity.Accident;
+import car_accident.entity.AccidentType;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class AccidentMem {
+
+    private Map<Integer, AccidentType> accidentTypes = new LinkedHashMap<>() {{
+        put(1, new AccidentType(1, "Две машины"));
+        put(2, new AccidentType(2, "Машина и пешеход"));
+        put(3, new AccidentType(3, "Машина и велосипедист"));
+    }};
+
     private Map<Long, Accident> accidents = new HashMap<>() {{
-            put(1L, new Accident(1L,"6.1 АК", "Столкновение", "Кутякова 65"));
-            put(2L, new Accident(2L,"7.8 АК", "Превышение скорости", "Московская 29/4"));
-            put(3L, new Accident(3L,"12.1 АК", "Езда по встречной полосе", "Полоумская 25"));
-            }};
+        put(1L, new Accident(1L,"6.1 АК", "Столкновение", "Кутякова 65",
+                accidentTypes.get(1)));
+        put(2L, new Accident(2L,"9.2 УК", "Сбил пешехода", "Московская 29/4",
+                accidentTypes.get(2)));
+        put(3L, new Accident(3L,"12.1 АК", "Езда по встречной полосе", "Полоумская 25",
+                accidentTypes.get(3)));
+    }};
 
     public Collection<Accident> findAllAccident() {
         return accidents.values();
     }
 
+    public Collection<AccidentType> findAllAccidentType() { return accidentTypes.values(); }
+
     public void saveOrUpdate(Accident accident) {
         if (accident.getId() != null) {
             accidents.put(accident.getId(), accident);
         } else {
-            long generatedKey = accidents.size() + 1;
-            accident.setId(generatedKey);
-            accidents.putIfAbsent(generatedKey, accident);
+            AtomicLong generatedKey = new AtomicLong(accidents.size());
+            long key = generatedKey.incrementAndGet();
+            accident.setId(key);
+            accidents.putIfAbsent(key, accident);
         }
     }
 
