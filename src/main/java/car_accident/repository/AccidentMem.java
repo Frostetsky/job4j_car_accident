d0321b48f6ed6291b6df4c6acc4192bda355ac63
Frostetskiy <nesterenko-nikita@bk.ru>
2021-06-27 13:14:07 +0300

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/car_accident/repository/AccidentMem.java
+++ src/main/java/car_accident/repository/AccidentMem.java
@@ -10,13 +10,13 @@ import java.util.concurrent.atomic.AtomicLong;
 @Repository
 public class AccidentMem {
 
-    private Map<Integer, AccidentType> accidentTypes = new LinkedHashMap<>() {{
+    private final Map<Integer, AccidentType> accidentTypes = new LinkedHashMap<>() {{
         put(1, new AccidentType(1, "Две машины"));
         put(2, new AccidentType(2, "Машина и пешеход"));
         put(3, new AccidentType(3, "Машина и велосипедист"));
     }};
 
-    private Map<Long, Accident> accidents = new HashMap<>() {{
+    private final Map<Long, Accident> accidents = new HashMap<>() {{
         put(1L, new Accident(1L,"6.1 АК", "Столкновение", "Кутякова 65",
                 accidentTypes.get(1)));
         put(2L, new Accident(2L,"9.2 УК", "Сбил пешехода", "Московская 29/4",
@@ -24,6 +24,8 @@ public class AccidentMem {
         put(3L, new Accident(3L,"12.1 АК", "Езда по встречной полосе", "Полоумская 25",
                 accidentTypes.get(3)));
     }};
+
+    private final AtomicLong generatedKey = new AtomicLong(accidents.size());
 
     public Collection<Accident> findAllAccident() {
         return accidents.values();
@@ -35,7 +37,6 @@ public class AccidentMem {
         if (accident.getId() != null) {
             accidents.put(accident.getId(), accident);
         } else {
-            AtomicLong generatedKey = new AtomicLong(accidents.size());
             long key = generatedKey.incrementAndGet();
             accident.setId(key);
             accidents.putIfAbsent(key, accident);
